ba920c0142c8d3212586ca9cabf2c4c2dd052e80
PancakeTAS <pancake@mgnet.work>
2023-12-23 20:00:09 +0100

    Write dead simple main class

---
+++ src/main/java/gay/pancake/asiochat/AsioChat.java
@@ -0,0 +1,65 @@
+package gay.pancake.asiochat;
+
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.StandardCopyOption;
+import java.util.Objects;
+
+/**
+ * Dead simple main class
+ *
+ * @author Pancake
+ */
+public class AsioChat {
+
+    public static void main(String[] args) throws Exception {
+        Files.copy(Objects.requireNonNull(AsioChat.class.getResourceAsStream("/jasiohost64.dll")), Path.of("jasiohost64.dll"), StandardCopyOption.REPLACE_EXISTING);
+
+        if (args.length == 0) {
+            System.out.println("Usage: java -jar asiochat.jar --client <ip> <port> | --server <port>");
+            return;
+        }
+
+        if (args[0].equals("--client")) {
+            if (args.length != 3) {
+                System.out.println("Usage: java -jar asiochat.jar --client <ip> <port>");
+                return;
+            }
+
+            var client = new Client("Focusrite USB ASIO");
+            client.connect(args[1], Integer.parseInt(args[2]));
+            client.start(0, 0, 1);
+
+            System.out.println("Press enter to exit...");
+            while (!client.getExitCallback().isCompletedExceptionally()) {
+                if (System.in.available() > 0 && System.in.read() == '\n')
+                    break;
+                Thread.yield();
+            }
+
+            client.stop();
+        } else if (args[0].equals("--server")) {
+            if (args.length != 2) {
+                System.out.println("Usage: java -jar asiochat.jar --server <port>");
+                return;
+            }
+
+            var server = new Server("Focusrite USB ASIO");
+            server.bind(Integer.parseInt(args[1]));
+            server.start(0, 0, 1);
+
+            System.out.println("Press enter to exit...");
+            while (!server.getExitCallback().isCompletedExceptionally()) {
+                if (System.in.available() > 0 && System.in.read() == '\n')
+                    break;
+                Thread.yield();
+            }
+
+            server.stop();
+        } else {
+            System.out.println("Usage: java -jar asiochat.jar --client <ip> <port> | --server <port>");
+        }
+
+    }
+
+}
